48fad7e43bb928df77977b12e50702a569921948
konhoe <wjstk7973@gmail.com>
2025-08-31 02:51:38 +0900

    노인역량활용사업 detail page

---
+++ src/app/(content)/projects/capacity-detail/mobile-admin/page.tsx
@@ -0,0 +1,20 @@
+// app/(content)/projects/capacity-detail/consumer-monitor/page.tsx
+import Image from "next/image";
+
+import OfficeImage from "@/assets/images/capacity/office.png";
+
+export default function Moblile() {
+  return (
+    <div className="flex justify-center">
+      <div className="relative w-[700px] h-auto">
+        <Image
+          src={OfficeImage}
+          alt="모바일도우미"
+          className="rounded-lg shadow"
+          width={700}
+          height={500}
+        />
+      </div>
+    </div>
+  );
+}
