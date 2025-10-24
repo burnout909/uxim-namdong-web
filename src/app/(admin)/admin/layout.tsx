5f988b009381c5b09b937a7529e1e0b471640329
burnout909 <znlwmdhkd1@yonsei.ac.kr>
2025-10-24 10:09:57 +0900

    delete admin sign=in

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -4,15 +4,15 @@ import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 
 export default function AdminLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-    const { loading, isAdmin, error } = useVerifyAdmin()
+    // const { loading, isAdmin, error } = useVerifyAdmin()
     const router = useRouter()
 
     //loading이 끝난 후 체크
-    useEffect(() => {
-        if (!loading && !isAdmin) {
-            router.replace('/admin/sign-in')
-        }
-    }, [loading, isAdmin, router])
+    // useEffect(() => {
+    //     if (!loading && !isAdmin) {
+    //         router.replace('/admin/sign-in')
+    //     }
+    // }, [loading, isAdmin, router])
     return (
         <div>
             {children}
